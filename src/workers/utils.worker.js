027429af569c0b34da506950e8ff937b7c4b8b74
Julien Calixte <julienc@bam.tech>
2020-03-29 21:28:03 +0200

    :recycle: (fibonacci) reuse fibonacci function in utils file

--- src/workers/utils.worker.js
+++ src/workers/utils.worker.js
@@ -1,12 +1,7 @@
+import { fibonacci } from '@/utils/utils'
+
 export const helloWorld = async () => {
   return 'Hello World from web worker'
-}
-
-const fibonacci = (count) => {
-  if (count === 0 || count === 1) {
-    return count
-  }
-  return fibonacci(count - 1) + fibonacci(count - 2)
 }
 
 export const wwFibonacci = async (count) => {
